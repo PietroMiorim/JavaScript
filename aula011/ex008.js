c7ffad3f5b5f30c0e6bc06acca66dff0e4653fd2
PietroMiorim <miorim.augusto@gmail.com>
2022-11-03 18:27:53 -0300

    fiz a aula011

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A velocidade é ${vel} Km/h`)
+if (vel > 60) { //Condição Simples!
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
